0f8d11940952a764e107c9e0a420b1348e2b3a6c
Maxim <evilrai27@gmail.com>
2021-09-19 09:28:05 +0000

    writer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Transport } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern('writer.handle.data')
-  handleData(data: any): any {
+  @MessagePattern('writer.handle.data.nats', Transport.NATS)
+  handleDataNATS(data: any): any {
+    console.log('received "writer.handle.data"');
+    return this.appService.handleData(data);
+  }
+
+  @MessagePattern({ cmd: 'writer.handle.data.tcp' }, Transport.TCP)
+  handleDataTCP(data: any): any {
     console.log('received "writer.handle.data"');
     return this.appService.handleData(data);
   }
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,31 @@ import { NATS_DEFAULT_URL } from '@nestjs/microservices/constants';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.NATS,
-      options: {
-        servers: [NATS_DEFAULT_URL],
-      },
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.NATS,
+  //     options: {
+  //       servers: [NATS_DEFAULT_URL],
+  //     },
+  //   },
+  // );
+  // await app.listen(() => {
+  //   console.log('Writer-service start');
+  // });
+  const app = await NestFactory.create(AppModule);
+  const microserviceNATS = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: [NATS_DEFAULT_URL],
     },
-  );
-  await app.listen(() => {
-    console.log('Writer-service start');
+  });
+  const microserviceTCP = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+  });
+  await app.startAllMicroservices();
+  await app.listen(3003, () => {
+    console.log('Writer-service started');
   });
 }
 bootstrap();
